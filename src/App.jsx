1fbf2340c932f62d590c607a2c7eba20350da5d2
vasilyefff <vasilyeff_k@mail.ru>
2026-01-23 15:29:42 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import AddTask from './components/AddTask/AddTask'
+import Board from './components/Board/Board'
+
+function App() {
+	return (
+		<>
+			<h1>Kanban Board</h1>
+			<AddTask />
+			<Board />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,11 @@
+import Column from "../Column/Column"
+
+export default function Board() {
+	return (
+		<div>
+			<Column />
+			<Column />
+			<Column />
+		</div>
+	)
+}
